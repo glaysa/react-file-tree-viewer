a956f38bd2c988bdb9f71e8ab300eca43b591b84
Glaysa <glaysa.df@gmail.com>
2021-04-18 16:44:20 +0200

    create basic filetreeview

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
+import {useState} from 'react'
 
 function App() {
-  return (
-    <p>Hello World!</p>
-  );
+    return (
+        <div className={'container my-5 p-3 border border-secondary'}>
+            <Folder name={'Documents'} isOpen={false}>
+                <File name={'dogs.png'}/>
+                <File name={'cats.png'}/>
+                <Folder name={'Projects'} isOpen={false}>
+                    <File name={'DNA_System.py'}/>
+                    <File name={'DNA_System32.bin'}/>
+                </Folder>
+            </Folder>
+            <Folder name={'Applications'} isOpen={false}>
+                <File name={'zombie.exe'}/>
+                <File name={'editor.jar'}/>
+            </Folder>
+        </div>
+    );
+}
+
+const Folder = (props) => {
+    const [isOpen, setIsOpen] = useState(false)
+    const {name, children} = props;
+    const folderSwitch = () => {setIsOpen(!isOpen)}
+
+    return (
+        <div>
+            <span onClick={folderSwitch}>{name}</span>
+            <div style={{marginLeft: "20px"}}>
+                {isOpen ? children : null}
+            </div>
+        </div>
+    );
+}
+
+const File = (props) => {
+    return (
+        <div>{props.name}</div>
+    );
 }
 
 export default App;
